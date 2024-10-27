a6805caad7a2fb81f9861b870424398b7740f878
Raed <raed.chammam@algolia.com>
2024-10-27 10:00:17 +0100

    add message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,16 @@ import React from "react";
 export default function Home() {
   return (
     <main>
-      <div className="mt-8 mx-auto px-4 sm:px-8">SongLink is now read-only</div>
+      <div className="mt-8 mx-auto px-4 sm:px-8">
+        SongLink is now read-only. Services will be shut down on{" "}
+        <a
+          className="text-blue-500"
+          href="https://github.com/raed667/songlink/pull/12"
+        >
+          Apr 2, 2025
+        </a>
+        .
+      </div>
     </main>
   );
 }
